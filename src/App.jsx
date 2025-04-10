6d824d0343714823ee1d0957044ee32951003f72
jaxdsout <jaxon.southern@mac.com>
2025-04-10 09:51:51 -0500

    lining up responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import { useState, useEffect } from "react";
 import Logo from "./Logo"
-import header from "./assets/455a9d0f85df81fcf8dc65019d274e4d.jpg"
-import header2 from "./assets/a8472ad2b5b3598dd3b576abff796d7b.jpg"
-import header3 from "./assets/eea55edc3749f89aa2c23cf508e55615.jpg"
+import b1 from "./assets/455a9d0f85df81fcf8dc65019d274e4d.jpg"
+import b2 from "./assets/a8472ad2b5b3598dd3b576abff796d7b.jpg"
+import b3 from "./assets/eea55edc3749f89aa2c23cf508e55615.jpg"
+import w1 from "./assets/8e080d9aec56de36e99359e9995e9606.jpg"
+import w2 from "./assets/5cb57ed69c2ddbbd3f496483c46e3a1d.jpg"
+import w3 from "./assets/d95fd3bdb487a9422716798f97117cdd.jpg"
 
-const backgrounds = [header, header2, header3];
+
+const workers = [w1, w2, w3];
+const backgrounds = [b1, b2, b3];
 
 function App() {
   const [lightning, setLightning] = useState(true);
-  const [stickyH, setStickyH] = useState(false);
+  const [sticky, setSticky] = useState(false);
   const [pusbBg, setPushBg] = useState(true);
+  const [carousel, setCarousel] = useState(0)
 
   setTimeout(() => {
     setPushBg(false)
   }, 2100)
 
   setTimeout(() => {
-    setStickyH(true)
+    setSticky(true)
   }, 2100)
 
   setTimeout(() => {
     setLightning(false)
   }, 2500)
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCarousel((prev) => (prev + 1) % backgrounds.length);
+    }, 9000);
+  
+    return () => clearInterval(interval);
+  }, []);
 
- 
 
 
   return (
@@ -60,7 +72,7 @@ function App() {
         </div>
       </div>
 
-      {!pusbBg && stickyH &&  (
+      {!pusbBg && sticky &&  (
           <div className="sticky top-0 z-40 bg-[#242424] w-full flex justify-center py-4 drop-shadow-lg">
             <Logo /> 
           </div>
@@ -68,27 +80,17 @@ function App() {
 
       {!pusbBg && (
         <div className='flex flex-col items-start justify-start h-screen'>
-          {/* <div className="relative w-full">
-            {backgrounds.map((bg, i) => (
-              <img
-                key={i}
-                src={bg}
-                alt={`Header ${i}`}
-                className="fade-slide absolute max-h-[500px] min-h-[350px] w-full object-cover"
-                style={{
-                  animationDelay: `${(i * 9) / backgrounds.length}s`,
-                  zIndex: backgrounds.length - i,
-                }}
-              />
-            ))}
-          </div> */}
-          <div className="flex flex-col items-center justify-center">
-            <div className="w-3/4 text-center mt-8 mb-8">
-                <p>At UpValley Electric Inc. we do our very best to cover all of your electrical needs. We pride ourselves on servicing wineries in addition to residential & commercial electrical work. There is never a job that is too big or too small for us. When you contact us, you can be sure that you are going to be met with a friendly, professional, and knowledgeable staff member. </p>
+          <div className="w-full">
+            <img src={backgrounds[carousel]} alt='Gallery' className="min-h-[350px] max-h-[500px] w-full object-cover"/>
+        
+          </div>
+          <div className="flex flex-col items-center justify-center inner-shadow drop-shadow-180">
+            <div className="w-7/8 sm:w-1/2 text-center mt-8 mb-8 bg-[#242424] rounded-lg p-6 drop-shadow-lg inner-shadow">
+                <p className="text-white">At UpValley Electric Inc. we do our very best to cover all of your electrical needs. We pride ourselves on servicing wineries in addition to residential & commercial electrical work. There is never a job that is too big or too small for us. When you contact us, you can be sure that you are going to be met with a friendly, professional, and knowledgeable staff member. </p>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="w-7/8 flex flex-col items-center justify-center mt-4 mb-8">
               <h1 className="text-xl font-bold tracking-wider mb-2">SERVICES</h1>
-              <ul className="grid grid-cols-2 gap-x-8 gap-y-2">
+              <ul className="grid grid-cols-2 gap-x-8 gap-y-2 text-center">
                   <li>New Construction & Remodel</li>
                   <li>Winery Refigeration Wiring</li>
                   <li>Thermal Imaging</li>
@@ -96,6 +98,9 @@ function App() {
                   <li>Energy Efficiency</li>
                   <li>Voice and Data Cabling</li>
               </ul>
+            </div>
+            <div className="w-7/8 sm:w-1/2 flex flex-col items-center justify-center mt-8 mb-8">
+              <img src={workers[carousel]} alt='Service' className="rounded-lg drop-shadow-lg"/>
             </div>
           </div>
           
@@ -136,7 +141,7 @@ function App() {
               </div>
             </div>
             <div className="mt-10 mb-5">
-              <p className="text-xs">UpValley Electric Inc. 2025</p>
+              <p className="text-xs">© 2025 UpValley Electric Inc.</p>
             </div>
           </div>
         </div>
---
+++ src/Switcher.jsx
@@ -0,0 +1,9 @@
+function Switcher () {
+
+    return (
+
+        <img alt="Patio with large glass sliding door at night with step and accent lights turned on" src="https%3A%2F%2Fimages.ctfassets.net%2Fvpu87v9zuty9%2F4YDJaguLV4c2kkVXpYc0UP%2F29f92409da70ec63bcc48592db52f573%2FPatio-Nocar-59145BKT-Overall-Night-4A-Landscape_1-1920x1080.jpg&amp;w=3840&amp;q=75"></img>
+    )
+}
+
+export default Switcher
